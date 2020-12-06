53ef88bdcbc1d9f07be799df176622bfcc715553
caleb turcotte <calebturcotte@cmail.carleton.ca>
2020-12-06 16:47:17 -0500

    merge fixes

--- Assets/SimpleEnemyBossBehavior.cs
+++ Assets/SimpleEnemyBossBehavior.cs
@@ -15,7 +15,7 @@ public class SimpleEnemyBossBehavior : MonoBehaviour
     private float bulletForce = 1f;
 
     private GameObject player;
-    private PlayerMovement enemyMove; //giving access to playerMovement
+    private Player enemyMove; //giving access to playerMovement
     private Rigidbody enemyrb;
 
     private bool patrolling;
@@ -38,7 +38,7 @@ public class SimpleEnemyBossBehavior : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player");
-        enemyMove = this.GetComponent<PlayerMovement>();
+        enemyMove = this.GetComponent<Player>();
         enemyrb = this.GetComponent<Rigidbody>();
         leftcheck = new Vector3(-1, -1, 0);
         rightcheck = new Vector3(1, -1, 0);
